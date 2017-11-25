b645d7b470173ecdd7e9f80bbe457554aab407bc
XIAO <t105360046@ntut.org.tw>
2017-11-25 15:57:28 +0800

    20171125_0357

--- STM32Cube_FW_F1_V1.4.0/Projects/STM3210E_EVAL/STM32-FINAL/Inc/display.h
+++ STM32Cube_FW_F1_V1.4.0/Projects/STM3210E_EVAL/STM32-FINAL/Inc/display.h
@@ -19,14 +19,36 @@ void LCD_Display(uint8_t point[][8])
 				LCD_Command = Set_ColL_Addr_X|coll;   delay();
 				while (k--)
 				{
+					uint8_t temp = 0;
+					if((k == 0)||(k == 1))
+					{
+						temp = 0xc3;
+						if((point[j][i] != point[j + 1][i])||(j == 15))
+						{
+							temp += 0x3c;
+						}
+					}
 					if((k == 2)||(k == 3)||(k == 4)||(k == 5))
 					{
-						LCD_Data = (point[j][i] * 0xc3);
+						temp = 0x00;
+						if((point[j][i] != point[j][i - 1])||(i == 0))
+						{
+							temp += 0xc0;
+						}
+						if((point[j][i] != point[j][i + 1])||(i == 7))
+						{
+							temp += 0x03;
+						}
 					}
-					else
+					if((k == 6)||(k == 7))
 					{
-						LCD_Data = (point[j][i] * 0xff);
+						temp = 0xc3;
+						if((point[j][i] != point[j - 1][i])||(j == 0))
+						{
+							temp += 0x3c;
+						}
 					}
+					LCD_Data = (point[j][i] * temp);
 					delay();
 				}
 				oldPoint[j][i] = point[j][i];
