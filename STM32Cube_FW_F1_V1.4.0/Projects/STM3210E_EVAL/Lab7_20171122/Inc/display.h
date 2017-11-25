eabef7e79a975fe37510ade41d1c53596b15d258
KAI <t105360045@ntut.org.tw>
2017-11-25 16:39:57 +0800

    KAI

--- STM32Cube_FW_F1_V1.4.0/Projects/STM3210E_EVAL/Lab7_20171122/Inc/display.h
+++ STM32Cube_FW_F1_V1.4.0/Projects/STM3210E_EVAL/Lab7_20171122/Inc/display.h
@@ -1,13 +1,18 @@
 #include "lcd.h"
-uint8_t oldPoint[16][8] = {0};
-void LCD_Display(uint8_t point[][8])
+
+uint8_t oldscreen[16][8] = 
+{
+	0
+};
+
+void LCD_Display(uint8_t screen[][8])
 {
 	uint8_t i, j, k = 0;
 	for(i = 0;i < 8;i++)
 	{
 		for(j = 0;j < 16;j++)
 		{
-			if(oldPoint[j][i] != point[j][i])
+			//if(oldscreen[j][i] != screen[j][i])
 			{
 				k = 8;
 				unsigned char coll = ((15 - j) * 8) & 0x0f;
@@ -21,16 +26,44 @@ void LCD_Display(uint8_t point[][8])
 				{
 					if((k == 2)||(k == 3)||(k == 4)||(k == 5))
 					{
-						LCD_Data = (point[j][i] * 0xc3);
+						LCD_Data = (screen[j][i] * 0xc3);
 					}
 					else
 					{
-						LCD_Data = (point[j][i] * 0xff);
+						LCD_Data = (screen[j][i] * 0xff);
 					}
 					delay();
 				}
-				oldPoint[j][i] = point[j][i];
+				oldscreen[j][i] = screen[j][i];
 			}
 		}
 	}
 }
+ 
+void clean_old(uint8_t array[4][4],uint8_t screen[16][8],uint8_t x,uint8_t y)
+{
+	for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				if(array[i][j])
+				{					
+					screen[i+y][j+x]=0;
+				}				
+			}
+		}
+}
+void set_new(uint8_t array[4][4],uint8_t screen[16][8],uint8_t x,uint8_t y)
+{
+	for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				if(array[i][j])
+				{					
+					screen[i+y][j+x]=1;
+				}				
+			}
+		}
+}
+
