b375a71018c5b3ca927e64fa47dc38f931278b8c
XIAO <t105360046@ntut.org.tw>
2017-11-27 00:52:00 +0800

    20171127_0051

--- STM32Cube_FW_F1_V1.4.0/Projects/STM3210E_EVAL/STM32-FINAL/Inc/display.h
+++ STM32Cube_FW_F1_V1.4.0/Projects/STM3210E_EVAL/STM32-FINAL/Inc/display.h
@@ -12,64 +12,60 @@ void LCD_Display(uint8_t screen[][8])
 	{
 		for(j = 0;j < 16;j++)
 		{
-			//if(oldscreen[j][i] != screen[j][i])
-			{
-				k = 8;
-				unsigned char coll = ((15 - j) * 8) & 0x0f;
-				unsigned char colh = ((15 - j) * 8) >> 4;
+			k = 8;
+			unsigned char coll = ((15 - j) * 8) & 0x0f;
+			unsigned char colh = ((15 - j) * 8) >> 4;
 		
-				LCD_Command = Set_Start_Line_X|0x0;   delay();
-				LCD_Command = Set_Page_Addr_X|i;  		delay();
-				LCD_Command = Set_ColH_Addr_X|colh;   delay();
-				LCD_Command = Set_ColL_Addr_X|coll;   delay();
-				while (k--)
+			LCD_Command = Set_Start_Line_X|0x0;   delay();
+			LCD_Command = Set_Page_Addr_X|i;  		delay();
+			LCD_Command = Set_ColH_Addr_X|colh;   delay();
+			LCD_Command = Set_ColL_Addr_X|coll;   delay();
+
+			while (k--)
+			{
+				uint8_t temp = 0;
+				if((k == 0)||(k == 1))
 				{
-					uint8_t temp = 0;
-					if((k == 0)||(k == 1))
+					temp = 0xc3;
+					if((screen[j][i] != screen[j - 1][i])||(j == 0))
 					{
-						temp = 0xc3;
-						if((screen[j][i] != screen[j - 1][i])||(j == 0))
-						{
-							temp += 0x3c;
-						}
+						temp += 0x3c;
 					}
-					if((k == 2)||(k == 3)||(k == 4)||(k == 5))
-					{
-						temp = 0x00;
-						if((screen[j][i] != screen[j][i + 1])||(i == 7))
-						{
-							temp += 0xc0;
-						}
-						if((screen[j][i] != screen[j][i - 1])||(i == 0))
-						{
-							temp += 0x03;
-						}
-					}
-					if((k == 6)||(k == 7))
-					{
-						temp = 0xc3;
-						if((screen[j][i] != screen[j + 1][i])||(j == 15))
-						{
-							temp += 0x3c;
-						}
-					}
-					if(screen[j][i] == 0)
-					{
-						temp = 0x00;
-					}
-					if(k == 7)
-					{
-						temp |= 0x01;
-						if(i == 7)
-						{
-							temp |= 0x80;
-						}
-					}
-					LCD_Data = temp;
-					delay();
 				}
-				oldscreen[j][i] = screen[j][i];
-				
+				if((k == 2)||(k == 3)||(k == 4)||(k == 5))
+			{
+					temp = 0x00;
+					if((screen[j][i] != screen[j][i + 1])||(i == 7))
+					{
+						temp += 0xc0;
+					}
+					if((screen[j][i] != screen[j][i - 1])||(i == 0))
+					{
+						temp += 0x03;
+					}
+				}
+				if((k == 6)||(k == 7))
+				{
+					temp = 0xc3;
+					if((screen[j][i] != screen[j + 1][i])||(j == 15))
+					{
+						temp += 0x3c;
+					}
+				}
+				if(screen[j][i] == 0)
+				{
+					temp = 0x00;
+				}
+				if(k == 7)
+				{
+					temp |= 0x01;
+					if(i == 7)
+					{
+						temp |= 0x80;
+					}
+				}
+				LCD_Data = temp;
+				delay();
 			}
 		}
 	}
